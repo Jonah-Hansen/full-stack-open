0b5963b030eedb9048ad7b401d0370442c082ac5
Jonah-Hansen <tce_mc@telus.net>
2022-05-04 16:15:53 -0600

    excersises 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,15 @@
+/**
+ * since my initial instinct for completing exercises 1.1 and 1.2
+ * used a loop to calculate total, I had already put parts into an
+ * array of objects. I see now I was a bit ahead of myself as that 
+ * would be part of 1.3-1.5!
+ * 
+ * this next commit will be updating the code to match the direction 
+ * of exercise 1.5
+ */
+
+
+
 const Header = (props) => (
   <h1>{props.course}</h1>     //display 'course' as a header.
 )
@@ -6,23 +18,23 @@ const Header = (props) => (
 // but I feel it is more readable and consistent like this.
 
 const Part = (props) => ( //returns the part title and excersizes on a new line
-  <p>{props.part.title} {props.part.exercises}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
 )
 
 const Content = (props) => {
   return (    //calls Part for each set in the array
   <div>    
-    <Part part={props.part[0]}/> 
-    <Part part={props.part[1]}/> 
-    <Part part={props.part[2]}/> 
+    <Part part={props.parts[0]}/> 
+    <Part part={props.parts[1]}/> 
+    <Part part={props.parts[2]}/> 
   </div>
   )
 }
 
 const Total = (props) => {
   let total = 0;
-  for (let x in props.part) {   //calculate the total by adding the value of exercises of each part
-    total += props.part[x].exercises;
+  for (let x in props.parts) {   //calculate the total by adding the value of exercises of each part. I know it says not to worry about using loops in 1.4 but I already set it up like this for 1.1 and 1.2 and I believe it looks nicer this way. 
+    total += props.parts[x].exercises;
   }
   return (
     <p>Number of exercises {total}</p>
@@ -30,18 +42,20 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part = [
-    {title: 'Fundamentals of React', exercises: 10},
-    {title: 'Using props to pass data', exercises: 7}, 
-    {title: 'State of a component', exercises: 14}
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7}, 
+      {name: 'State of a component', exercises: 14}
+    ]
+  }
   
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part}/>
-      <Total part={part}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
