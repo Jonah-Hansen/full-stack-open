4c028a1ef4b4b5e47e7628e81ed613cb4c979e7d
Jonah-Hansen <tce_mc@telus.net>
2022-05-07 19:52:30 -0600

    exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,11 +4,20 @@ const App = () => {
   //save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [nad, setBad] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-
+      <h1>give feedback</h1> 
+      {/* render 3 buttons, with even handlers that increase their respective state by 1 */}
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      {/* render the value of each state */}
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
