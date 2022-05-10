98f987e7ad0ebc82aafb3daaf571b2ea8c006455
Jonah-Hansen <tce_mc@telus.net>
2022-05-10 17:54:09 -0600

    exercise 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,49 +1,31 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+import Course from './components/Course'
 
 const App = () => {
   const course = {
-    id: 1,
+    id: '1',
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
-        id: 1
+        id: '1'
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
-        id: 2
+        id: '2'
       },
       {
         name: 'State of a component',
         exercises: 14,
-        id: 3
+        id: '3'
       }
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <Course course={course} />
+  )
 }
 
 export default App
